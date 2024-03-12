52930f004334e7459f6ebb8340ad6a3903de9874
nabiltopo1 <nabil.boutrik1@gmail.com>
2024-03-12 04:06:56 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,431 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama1",
+      "name": "PANORAMA1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -1.412899874698514,
+        "pitch": 0.3884793294031539,
+        "fov": 1.041196084513444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.106250828528305,
+          "pitch": 0.5528581807559476,
+          "rotation": 0.7853981633974483,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": -2.009430394487751,
+          "pitch": 0.3126783001258122,
+          "rotation": 7.853981633974483,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": -1.9292277111333735,
+          "pitch": 0.17908651951547583,
+          "rotation": 1.5707963267948966,
+          "target": "3-panorama4"
+        },
+        {
+          "yaw": -0.664072907860584,
+          "pitch": -0.015366395729335736,
+          "rotation": 3.141592653589793,
+          "target": "5-panorama6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9464577979654898,
+          "pitch": 0.9850081993652111,
+          "title": "Concasseurs",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama2",
+      "name": "PANORAMA2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9946905019404397,
+          "pitch": 0.18365795530058193,
+          "rotation": 4.71238898038469,
+          "target": "0-panorama1"
+        },
+        {
+          "yaw": -1.7323959699287492,
+          "pitch": 0.2825044536238366,
+          "rotation": 7.853981633974483,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": -1.98365819302472,
+          "pitch": 0.23206811061772115,
+          "rotation": 13.351768777756625,
+          "target": "3-panorama4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5204918974825894,
+          "pitch": 0.454679398309775,
+          "title": "Concasseurs",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9682355735006887,
+          "pitch": 0.14175016242202787,
+          "title": "<div>Arrêté n°68/2019&nbsp;</div><div><br></div>",
+          "text": "Sté SGTM"
+        },
+        {
+          "yaw": -0.7832373691887771,
+          "pitch": 0.42788982658194463,
+          "title": "<div>Arrêté n°65/2017&nbsp;</div><div><div>Avenant N°1_2018</div><div><br></div><div>Arrêté n°65/2017&nbsp;</div></div><div><br></div><div><br></div>",
+          "text": "Sté BOUDRAHEM"
+        }
+      ]
+    },
+    {
+      "id": "2-panorama3",
+      "name": "PANORAMA3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3492539890432802,
+          "pitch": 0.231666485670317,
+          "rotation": 5.497787143782138,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": 1.1817451138555661,
+          "pitch": 0.03468353167005844,
+          "rotation": 5.497787143782138,
+          "target": "0-panorama1"
+        },
+        {
+          "yaw": -1.860876091254049,
+          "pitch": 0.3141389920565505,
+          "rotation": 1.5707963267948966,
+          "target": "3-panorama4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.32787594762359795,
+          "pitch": 0.1814687430991846,
+          "title": "<div>Arrêté n°68/2019&nbsp;</div><div><br></div>",
+          "text": "Sté SGTM"
+        },
+        {
+          "yaw": -1.934586283068791,
+          "pitch": 0.4473358190150165,
+          "title": "Bureaux et instalations",
+          "text": "SGTM"
+        }
+      ]
+    },
+    {
+      "id": "3-panorama4",
+      "name": "PANORAMA4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3441648818429961,
+          "pitch": 0.12159240840172814,
+          "rotation": 5.497787143782138,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": -0.5483918239440477,
+          "pitch": 0.014393252628483566,
+          "rotation": 0,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": -2.023477078736228,
+          "pitch": -0.014308383570448058,
+          "rotation": 1.5707963267948966,
+          "target": "4-panorama5"
+        },
+        {
+          "yaw": -1.655734845524064,
+          "pitch": -0.14710688810860972,
+          "rotation": 3.141592653589793,
+          "target": "5-panorama6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6883708177667085,
+          "pitch": 0.5321972661543466,
+          "title": "Entré de la carrière",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4344541215186375,
+          "pitch": 1.3505527946630966,
+          "title": "Bureaux et instalations",
+          "text": "SGTM"
+        },
+        {
+          "yaw": -1.3385180141628723,
+          "pitch": 0.04101534289782016,
+          "title": "<div>Arrêté n°68/2019&nbsp;</div><div><br></div>",
+          "text": "SGTM"
+        }
+      ]
+    },
+    {
+      "id": "4-panorama5",
+      "name": "PANORAMA5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.629248193706397,
+          "pitch": -0.2809238099420277,
+          "rotation": 3.141592653589793,
+          "target": "5-panorama6"
+        },
+        {
+          "yaw": -0.42992343440039527,
+          "pitch": -0.00062782024240704,
+          "rotation": 3.141592653589793,
+          "target": "3-panorama4"
+        },
+        {
+          "yaw": -0.6922101694252003,
+          "pitch": 0.010867160802321862,
+          "rotation": 4.71238898038469,
+          "target": "2-panorama3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5223644128803713,
+          "pitch": 0.2797647773256582,
+          "title": "Entré de la carrière",
+          "text": "BOUDRAHEM"
+        }
+      ]
+    },
+    {
+      "id": "5-panorama6",
+      "name": "PANORAMA6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9160393741390322,
+          "pitch": 0.509616310700288,
+          "rotation": 5.497787143782138,
+          "target": "4-panorama5"
+        },
+        {
+          "yaw": 2.2496959320435908,
+          "pitch": 0.2565060215793835,
+          "rotation": 3.141592653589793,
+          "target": "3-panorama4"
+        },
+        {
+          "yaw": 2.04958020177283,
+          "pitch": 0.24198665802723873,
+          "rotation": 3.141592653589793,
+          "target": "2-panorama3"
+        },
+        {
+          "yaw": 1.709958020530836,
+          "pitch": 0.2534497975824195,
+          "rotation": 3.141592653589793,
+          "target": "1-panorama2"
+        },
+        {
+          "yaw": 1.4624032544962322,
+          "pitch": 0.22126300587012793,
+          "rotation": 3.141592653589793,
+          "target": "0-panorama1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.17356993531421594,
+          "pitch": 1.110073807436855,
+          "title": "Arrêté n°68/2017",
+          "text": "BOUDRAHEM"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
